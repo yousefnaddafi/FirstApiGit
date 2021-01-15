9cb0d76e7f110d3d3a499b7f2c1e930fde4035f7
yousefnaddafi <yousefnaddafi@gmail.com>
2021-01-15 09:12:01 +0330

    Add project files.

---
+++ FirstApiGit/Controllers/FoodController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FoodReciepe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        //private readonly HttpClient Client;
+        //private const string BaseAdress = "";
+        //private const string ApiKey = "";
+        //public FoodController(HttpClient client)
+        //{
+        //    this.Client = client;
+        //    this.Client.BaseAddress = new Uri(BaseAdress);
+        //    this.Client.DefaultRequestHeaders.Add("Accept","application/json");
+        //}
+        //public Food GetReciepe(string food);
+        //Environm
+    }
+}
