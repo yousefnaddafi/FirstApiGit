cadb65fa07c95240da314545f1bca30693a2b0f5
foroghasgariii@gmail.com <foroghasgariii@gmail.com>
2021-01-15 20:04:19 +0330

    dfbfd

---
+++ FirstApiGit/Models/NEw.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodReciepe.Models
+{
+    public class NEw
+    {
+        public string name { get; set; }
+    }
+}
